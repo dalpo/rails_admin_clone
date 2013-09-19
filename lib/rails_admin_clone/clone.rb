7dee845de8af757de10fe832dd61589f5ccfe8f4
Andrea Dal Ponte <info@andreadalponte.com>
2013-09-19 09:22:54 +0200

    exlude fields

--- lib/rails_admin_clone/clone.rb
+++ lib/rails_admin_clone/clone.rb
@@ -19,8 +19,12 @@ module RailsAdmin
             @old_object = @object
             @object = @abstract_model.new
 
+            attributes = @old_object.attributes.select do |k,v|
+              !['id', 'created_at', 'updated_at'].include?(k)
+            end
+
             # clone old_object
-            @object.assign_attributes @old_object.attributes, without_protection: true
+            @object.assign_attributes attributes, without_protection: true
 
             # clone has_one associations
             @object.class.reflect_on_all_associations(:has_one).each do |class_association|
@@ -29,7 +33,7 @@ module RailsAdmin
 
               if old_association
                 attributes = old_association.attributes.select do |k,v|
-                  !['id', 'created_at', 'created_at', class_association.try(:foreign_key), class_association.try(:type)].include?(k)
+                  !['id', 'created_at', 'updated_at', class_association.try(:foreign_key), class_association.try(:type)].include?(k)
                 end
 
                 @object.send(:"build_#{association_name}").tap do |a|
@@ -44,13 +48,21 @@ module RailsAdmin
 
               @old_object.send(association_name).each do |old_association|
                 attributes = old_association.attributes.select do |k,v|
-                  !['id', 'created_at', 'created_at', class_association.try(:foreign_key), class_association.try(:type)].include?(k)
+                  !['id', 'created_at', 'updated_at', class_association.try(:foreign_key), class_association.try(:type)].include?(k)
                 end
 
                 @object.send(association_name).build.tap do |a|
                   a.assign_attributes old_association.attributes, without_protection: true
                 end
               end
+            end
+
+            # clone has_and_belongs_to_many associtations
+            @object.class.reflect_on_all_associations(:has_and_belongs_to_many).each do |class_association|
+              association_name = class_association.name
+              association_name_ids = association_name.to_s.singularize.to_sym
+
+              @object.send(association_name_ids, @old_object.send(association_name_ids))
             end
 
             @authorization_adapter && @authorization_adapter.attributes_for(:new, @abstract_model).each do |name, value|
